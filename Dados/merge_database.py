1229cf2273d4bad3fde2083998fd46bc8abcd34c
vitorbborges <vitorbborges@terra.com.br>
2022-07-21 00:26:33 -0300

    3

--- Dados/merge_database.py
+++ Dados/merge_database.py
@@ -7,4 +7,5 @@ df=pd.DataFrame()
 for i in os.listdir(partial_folder):
     dfp = pd.read_excel(partial_folder + i)
     df = pd.concat([df,dfp])
-df.to_excel('journals.xls')
+    
+df.to_excel('journals.xlsx')
