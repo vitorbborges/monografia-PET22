b656d4b6042fc263f7f45eb0bc214257b3eb821b
vitorbborges <vitorbborges@terra.com.br>
2022-07-21 14:29:37 -0300

    8

--- database/merge_database.py
+++ database/merge_database.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 
 partial_folder = os.getcwd() + '\\journals-partial\\'
 df=pd.DataFrame()
@@ -8,4 +9,6 @@ for i in os.listdir(partial_folder):
     dfp = pd.read_excel(partial_folder + i)
     df = pd.concat([df,dfp])
     
-df.to_csv('journals.csv', index = False)
+df = df.iloc[np.where(~df['Abstract'].isna())]
+    
+df.to_csv('journals.csv', index=False)
